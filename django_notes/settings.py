11c762b493ff88fda038b9b057b101754870e3f7
bdelgado1994 <59299188+bdelgado1994@users.noreply.github.com>
2023-06-22 23:13:49 -0600

    secret key

--- django_notes/settings.py
+++ django_notes/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ(
-    "SELCRET_KEY", default="yor secret key"
+    "SELCRET_KEY",
+    default="django-insecure-8@y5hchv_8(6)+2&)p)z5lfw^2bz*^1q0704&^c1gwkrc!8fr_",
 )  # "django-insecure-8@y5hchv_8(6)+2&)p)z5lfw^2bz*^1q0704&^c1gwkrc!8fr_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
