26e472be0121cc557fb71a8f74d9a598b5ccba78
bdelgado1994 <59299188+bdelgado1994@users.noreply.github.com>
2023-06-22 23:17:57 -0600

    s

--- django_notes/settings.py
+++ django_notes/settings.py
@@ -22,10 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ(
-    "SELCRET_KEY",
-    default="django-insecure-8@y5hchv_8(6)+2&)p)z5lfw^2bz*^1q0704&^c1gwkrc!8fr_",
-)  # "django-insecure-8@y5hchv_8(6)+2&)p)z5lfw^2bz*^1q0704&^c1gwkrc!8fr_"
+SECRET_KEY = "django-insecure-8@y5hchv_8(6)+2&)p)z5lfw^2bz*^1q0704&^c1gwkrc!8fr_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
